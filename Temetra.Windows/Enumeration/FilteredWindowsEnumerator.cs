7ca4fd848bc8ebe0e35d0c2f1d8ace3e6f105f1a
Temetra <git@temetra.cc>
2024-11-07 19:21:37 +0000

    Handle missing program data, return more info

--- Temetra.Windows/Enumeration/FilteredWindowsEnumerator.cs
+++ Temetra.Windows/Enumeration/FilteredWindowsEnumerator.cs
@@ -5,10 +5,32 @@ using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace Temetra.Windows;
 
+public enum EnumerationResultStatus
+{
+    Successful,
+    Excluded,
+    Invisible,
+    Cloaked,
+    ToolWindow,
+    NoDetails
+}
+
+public class EnumerationResult
+{
+    // Status of the query
+    public EnumerationResultStatus Status { get; set; }
+
+    // Context for unsuccessful query - HWND or file path
+    public string? Context { get; set; }
+
+    // Programe details if query successful
+    public ProgramDetails? Details { get; set; }
+}
+
 public static class FilteredWindowsEnumerator
 {
     // EnumWindows continues until the last window is enumerated or the callback returns false
-    public static void EnumWindows(Func<ProgramDetails, bool> callback)
+    public static void EnumWindows(Func<EnumerationResult, bool> callback)
     {
         BOOL lpEnumFunc(HWND hwnd, LPARAM lparam)
         {
@@ -20,17 +42,39 @@ public static class FilteredWindowsEnumerator
         PInvoke.EnumWindows(lpEnumFunc, nint.Zero);
     }
 
-    private static ProgramDetails? GetWindowItem(HWND hwnd)
+    private static EnumerationResult GetWindowItem(HWND hwnd)
     {
+
         // Exclude this application
         PInvokeHelpers.GetWindowThreadProcessId(hwnd, out uint processId);
-        if (processId == Environment.ProcessId) return null;
+        if (processId == Environment.ProcessId)
+        {
+            return new EnumerationResult
+            {
+                Status = EnumerationResultStatus.Excluded,
+                Context = hwnd.ToString()
+            };
+        }
 
         // Exclude windows without the Visible style
-        if (!PInvoke.IsWindowVisible(hwnd)) return null;
+        if (!PInvoke.IsWindowVisible(hwnd))
+        {
+            return new EnumerationResult
+            {
+                Status = EnumerationResultStatus.Invisible,
+                Context = hwnd.ToString()
+            };
+        }
 
         // Ignore windows that are cloaked
-        if (PInvokeHelpers.IsWindowCloaked(hwnd)) return null;
+        if (PInvokeHelpers.IsWindowCloaked(hwnd))
+        {
+            return new EnumerationResult
+            {
+                Status = EnumerationResultStatus.Cloaked,
+                Context = hwnd.ToString()
+            };
+        }
 
         // Check windows without WS_EX_APPWINDOW
         var windowStylesEx = PInvokeHelpers.GetWindowStyleEx(hwnd);
@@ -38,12 +82,26 @@ public static class FilteredWindowsEnumerator
         {
             // Exclude tool windows, and windows that don't become foreground window when clicked
             var flags = WINDOW_EX_STYLE.WS_EX_TOOLWINDOW | WINDOW_EX_STYLE.WS_EX_NOACTIVATE;
-            if ((windowStylesEx & flags) != 0) return null;
+            if ((windowStylesEx & flags) != 0)
+            {
+                return new EnumerationResult
+                {
+                    Status = EnumerationResultStatus.ToolWindow,
+                    Context = hwnd.ToString()
+                };
+            }
         }
 
         // Create program details
         var filename = PInvokeHelpers.GetFilenameForProcess(processId);
         var item = ProgramDetails.GetFromFilename(filename);
+
+        // Failed to get details, probably permissions issue
+        if (item == null) return new EnumerationResult
+        {
+            Status = EnumerationResultStatus.NoDetails,
+            Context = filename
+        };
 
         // If exe is ApplicationFrameHost, check children for details
         if (item.Executable == "ApplicationFrameHost.exe")
@@ -64,11 +122,11 @@ public static class FilteredWindowsEnumerator
 
                 return true;
             }
-            
+
             // Process child windows
             PInvoke.EnumChildWindows(hwnd, lpEnumFunc, nint.Zero);
         }
 
-        return item;
+        return new EnumerationResult { Status = EnumerationResultStatus.Successful, Details = item };
     }
 }
